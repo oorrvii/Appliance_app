02e4447c8419e2762e9bae613304048def318783
oorrvii <oorvikulshreshtha15@gmail.com>
2026-03-27 13:52:02 +0530

    connected to mongodb

---
+++ db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config();
+
+const connectDB = async()=>{
+    try{
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("succesfully connected mongodb")
+    }catch(error){
+        console.log("Error connecting to mongodb",error)
+    }
+    }
+export default connectDB;
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+import connectDB from './db.js';
+
 const app=express();
 app.get('/',(req,res)=>{
     res.send('Hello World');
 });
-
-app.listen(process.env.PORT,()=>{
-    console.log(`Server is running on port ${process.env.PORT}`);
-});
+// this here means first connnect to database then only start listening to server.
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server is running on port ${process.env.PORT}`)
+    })
+})
